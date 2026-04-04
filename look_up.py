c472209388d79bbb4d5192ca048d0a9a84d75086
EgorSavchenko-web <a9654626435@gmail.com>
2026-04-04 16:09:09 +0300

    Add TO DO

--- look_up.py
+++ look_up.py
@@ -5,5 +5,5 @@ proxy = xmlrpc.client.ServerProxy("http://localhost:8000", allow_none=True)
 proxy.add("test", "000")
 """
 stmt = 'proxy.lookup("test")'
-latency = timeit.timeit(stmt, setup=setup, number=1000) / 1000
-print(f"Average lookup latency: {latency*1000:.2f} ms")
+latency = timeit.timeit(stmt, setup=setup, number=100) / 100
+print(f"Average lookup latency: {latency*100:.2f} ms")
